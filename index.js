92eaf1f8b4831004053ca4f6242e527ede8d8031
João Pedro Pagno <pagno.jp@gmail.com>
2019-07-30 15:24:59 -0300

    part I Routes

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+
+app.set('view engine', 'njk')
+
+app.get('/', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/check', (req, res) => {
+  let age = req.body.age
+  if (age >= 18) {
+    return res.render('major', { age })
+  } else {
+    return res.render('minor', { age })
+  }
+})
+
+app.listen(3000)
